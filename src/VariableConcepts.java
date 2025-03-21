937872fddf95699c5550bd8d54495efec7a04a69
Ashish Mishra <ashish.mishra.sse@gmail.com>
2025-03-21 09:10:20 +0530

    Primitive Data Types - Part 1

---
+++ src/VariableConcepts.java
@@ -0,0 +1,29 @@
+public class VariableConcepts {
+    public static void main(String[] args) {
+
+        //10
+
+        byte a = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 2147483648L;
+
+//        System.out.println(a);
+//        System.out.println(s);
+//        System.out.println(i);
+//        System.out.println(l);
+
+        float f =10.123456789F; //4 Byte, up to 6-7 decimal places
+        System.out.println(f);
+        double d =10.1234567890123456789; //8 Byte, beyond 6-7 decimal places, up-to 15 decimal Places
+        System.out.println(d);
+
+
+
+
+
+
+
+
+    }
+}
